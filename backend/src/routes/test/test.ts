f677c5e02628567961a7615313d49d2b0f1c967a
mmar58 <rahmanapu118@gmail.com>
2026-08-12 15:41:28 +0600

    Updated api endpoint to response always nan if not number

--- backend/src/routes/test/test.ts
+++ backend/src/routes/test/test.ts
@@ -2,24 +2,15 @@ import type { FastifyPluginAsync } from "fastify";
 import { lcm } from "./testService";
 const testRoutes: FastifyPluginAsync = async (fastify) => {
     fastify.get("/", async (request, reply) => {
-        const { x, y } = request.query as { x: string, y: string }
+        const { x, y } = request.query as { x: bigint, y: bigint }
         console.group("Test lcm for " + request.ip)
         console.log("Received x", x, "y", y)
-        // return x + " " + y
-        if (!x || !y) {
-            reply.status(400).send({ message: "Invalid input" });
-        }
-        let numberX = 0n, numberY = 0n;
-
-        if (x.includes("{") || y.includes("{") || x.includes("}") || y.includes("}")) {
+        // return if x and y isn't number
+        if (!Number.isInteger(x) || !Number.isInteger(y)) {
             console.log("Result NaN")
             return "NaN"
         }
-        else {
-            numberX = BigInt(x)
-            numberY = BigInt(y)
-        }
-        let result = lcm(numberX, numberY)
+        let result = lcm(x, y)
         console.log("Result", result.toString())
         console.groupEnd()
         return result.toString();
